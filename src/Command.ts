0c490d837c9dfc989f0461f9861fdebb30f907d6
Nurik Nurmetov <nurmakhamad.nurmetov@sidenis.com>
2020-07-14 02:59:20 +0300

    feat: initial

---
+++ src/Command.ts
@@ -0,0 +1,124 @@
+import * as yargs from 'yargs';
+
+export class Command {
+    private _validatorFn: (self: Command) => void;
+    private _transformerFn: (self: Command) => void;
+    private _cli: typeof yargs;
+    private _transformedValue: any;
+    private _dependent: Command = null;
+    public constructor(public name: string = '', public alias: string = '', public type: 'string' | 'number' | 'boolean') {
+        if (this.name === '' || this.alias === '') {
+            throw new Error('name and alias properties must not be empty string');
+        }
+    }
+    public set cli(v: typeof yargs) {
+        this._cli = v;
+    }
+    public init() {
+        if (!this._cli) {
+            throw new Error('yargs is required');
+        }
+        this.valueType = this.type;
+        this._cli.option(this.getName(), {
+            alias: this.getAlias()
+        });
+    }
+    public set defaultValue(value: any) {
+        this._cli.default(this.getName(), value);
+    }
+    public set required(v: boolean) {
+        if (v) {
+            this._cli.required(this.getName(), `Option ${this.name} is required`);
+        }
+    }
+    public set valueType(type: 'string' | 'number' | 'boolean') {
+        switch (type) {
+            case 'string':
+                this._cli.string(this.getName());
+                break;
+            case 'number':
+                this._cli.number(this.getName());
+                break;
+            case 'boolean':
+                this._cli.boolean(this.getName());
+                break;
+            default: throw new Error(`Unknown type: ${type}`);
+        }
+    }
+    getName(): string {
+        return this.extractName(this.name);
+    }
+    getAlias(): string {
+        return this.extractName(this.alias);
+    }
+    getInputArgs() {
+        return this._cli.argv;
+    }
+    getInputValue(): any {
+        return this._cli.argv[this.getName()];
+    }
+    private extractName(str: string) {
+        return str.replace(/^[\-]*$/, '');
+    }
+    public setValidator(fn: (self: Command) => any) {
+        this._validatorFn = fn.bind(this);
+        return this;
+    }
+    public setTransformerValue(fn: (inputValue: any) => any) {
+        this._transformerFn = fn.bind(this);
+        return this;
+    }
+    public transformValue() {
+        if (this._transformerFn !== undefined && this.getInputValue() !== undefined) {
+            this._transformedValue = this._transformerFn.call(this, this.getInputValue());
+        }
+        else {
+            this._transformedValue = this.getInputValue();
+        }
+    }
+    public async validate() {
+        if (typeof this._validatorFn === 'undefined') {
+            this._validatorFn = async (self: Command) => { };
+        }
+        return this._validatorFn(this);
+    }
+
+    public async handle() {
+        if(!(!this.required && typeof this.getInputValue() === 'undefined')){
+            try {
+                await this.validate();
+                return this.transformValue();
+            } catch(err){
+                throw err;
+            }
+        }
+    }
+
+    public getValue() {
+        return this._transformedValue || null;
+    }
+    public count() {
+        this._cli.count(this.getName());
+        return this;
+    }
+
+    public dependsOn(command: Command) {
+        this._dependent = command;
+    }
+
+    public get dependent() {
+        return this._dependent;
+    }
+}
+
+function safe<T>(obj): T {
+    return new Proxy(obj, {
+        get: function (target, name) {
+            const result = target[name];
+            if (!!result) {
+                return (result instanceof Object) ? safe(result) : result;
+            }
+            return safe({});
+        }
+    });
+}
---
+++ src/app.ts
@@ -0,0 +1,123 @@
+import { Cli } from './Cli';
+import * as execa from 'execa';
+import * as fsx from 'fs-extra';
+import * as path from 'path';
+const Table = require('cli-table');
+
+const log = console.log;
+export async function main() {
+    const app = new Cli();
+
+    const inputCommand = app
+        .add(false, 'input', 'i', 'string')
+        .setValidator(async(self) => {
+            await fsx.access(self.getInputValue());
+            const pathToRepo = path.resolve(self.getInputValue(),'.git');
+            await fsx.access(pathToRepo);
+        })
+        .setTransformerValue(value => {
+            return path.resolve(value);
+        });
+    app.add(false, 'plain', 'p', 'boolean');
+    app.add(false, 'json', 'j', 'boolean');
+    app
+        .add(false, 'for-commit', 'f', 'string')
+        .setValidator(async(self) => {
+            try {
+                const commitHash = self.getInputValue();
+                await execa(`git cat-file -e ${commitHash}`, {cwd: self.dependent.getValue()});
+            } catch(err){
+                console.log(err);
+                throw new Error(`Commit ${self.getInputValue()} not found!`);
+            }
+        }).setTransformerValue(value => {
+            return value || null;
+        }).dependsOn(inputCommand);
+    await app.run();
+    try {
+        let projectPath = app.getCommand('input').getValue();
+        const isPlain = app.getCommand('plain').getValue();
+        const isJson = app.getCommand('json').getValue();
+        const hash = app.getCommand('for-commit').getValue();
+        if (!projectPath) {
+            projectPath = path.resolve(process.cwd(), '.git');
+            console.log(projectPath);
+            try {
+                await fsx.access(projectPath);
+            } catch(err) {
+                return console.log('There is no .git directory in current directory. Please use --input[-i] option if it in another place');
+            }
+        }
+        
+        // process.chdir(projectPath);
+        // const {stdout, stderr} = await execa(`git log --pretty=format:"%H" | xargs git show --relative --pretty=format:'%ae %s' --name-only`);
+        let gitShowResult;
+        if(hash){
+            gitShowResult = await execa('xargs', ['git', '--no-pager', 'show', '--decorate=short', '--relative', '--pretty=format:">>>%ae|%s|%h"', '--name-only'], {input: hash, cwd: projectPath}); 
+        } else {
+            let gitLogResult = execa('git', ['--no-pager', 'log', '--decorate=short', '--pretty=format:"%H"'], {stdout: 'pipe', cwd: projectPath});
+            gitShowResult = await execa('xargs',['git', '--no-pager', 'show', '--decorate=short', '--relative', '--pretty=format:">>>%ae|%s|%h|%cr"', '--name-only'], {input: gitLogResult.stdout, cwd: projectPath});
+        }
+        let spliitedbyCommits = gitShowResult.stdout.split('>>>').slice(1).map(commit => {
+            let newCommit = { author: '', name: '', files: [], hash: '', date: '' };
+            const [msg, ...files] = commit.split('\n');
+            const [author, name, hash, date] = msg.split('|');
+            newCommit.author = author;
+            newCommit.name = name;
+            newCommit.files = files;
+            newCommit.hash = hash.replace('\"','');
+            newCommit.date = date;
+            return newCommit;
+        });
+
+        
+        const result = spliitedbyCommits
+            // .map(commit => ({ [commit.author]: commit.files.length }))
+            .reduce((total, commit) => {
+                if((commit.author in total)){
+                    total[commit.author] += commit.files.length;
+                } else {
+                    total[commit.author] = commit.files.length;
+                }
+                return total;
+            }, Object.create(null))
+        ;
+
+        // const result = spliitedbyCommits
+        
+        if(isPlain){
+            console.log(
+                spliitedbyCommits
+                    .forEach(commit => console.log(`${commit.author}\t${commit.name}\t${commit.files.length}\t${commit.hash}\t${commit.date}`))
+            );
+        } else if(isJson){
+            console.log(
+                JSON.stringify(
+                    spliitedbyCommits
+                        .map(commit => ({name: commit.name, author: commit.author, files: commit.files.length, hash: commit.hash, date: commit.date})),
+                        null,
+                        2
+                )
+            );
+        } else {
+            const table= new Table({
+                head: ['Author','Commit','Changed Files', 'hash', 'date'],
+                colWidths: [50, 50, 13, 10, 20]
+            });
+            spliitedbyCommits.forEach(commit => table.push([commit.author, commit.name, commit.files.length, commit.hash, commit.date]));
+            console.log(table.toString());
+        }
+
+        // Object.keys(result).forEach(author => table.push([author, result[author]]));
+        // render(result);
+        // spliitedbyCommits.forEach(commit => process.stdout.write(`${commit.author}: ${commit.files.length}\n`));
+    } catch(err){
+        log(err);
+    }
+}
+
+function render(input: object){
+    for(let key in input){
+        console.log(`${key}: ${input[key]}`);
+    }
+}
