96d0f3548ae69476fd96c0b1d58f9f6d96ba214f
nurik <nurik6@gmail.com>
2021-05-28 18:15:41 +0500

    refactor: update

--- src/Cli.ts
+++ src/Cli.ts
@@ -1,74 +1,66 @@
-import { Command } from "./Command";
-import * as yargs from "yargs";
+import { Command } from './Command';
+import * as yargs from 'yargs';
 
 export class Cli {
-  private _commands: Command[] = [];
-  private _validationErrors: Error[] = [];
-  private cli: typeof yargs = yargs;
+	private _commands: Command[] = [];
+	private _validationErrors: Error[] = [];
+	private cli: typeof yargs = yargs;
 
-  public get yargs(): typeof yargs {
-    return this.cli;
-  }
-  public add(
-    required?: boolean,
-    ...args: ConstructorParameters<typeof Command>
-  ) {
-    const command = new Command(...args);
-    command.cli = this.cli;
-    command.init();
-    if (required) {
-      command.required = true;
-    }
-    this._commands.push(command);
-    return command;
-  }
-  public async run(): Promise<void> {
-    try {
-      this._sortByDependent();
-      for (let index in this._commands) {
-        await this._commands[index].handle.call(this._commands[index]);
-      }
-    } catch (err) {
-      this._validationErrors.push(err);
-      this._printErrors();
-      process.exit(1);
-    }
-  }
-  private _printErrors() {
-    this._validationErrors.forEach((error) => console.log(error));
-  }
-  public getCommand(name: string): InstanceType<typeof Command> {
-    const result = this._commands.filter(
-      (command) => command.getName() === name
-    );
-    if (result.length <= 0) {
-      throw new Error(`Command with name: ${name} not found!`);
-    }
-    return result[0];
-  }
+	public get yargs(): typeof yargs {
+		return this.cli;
+	}
+	public add(required?: boolean, ...args: ConstructorParameters<typeof Command>) {
+		const command = new Command(...args);
+		command.cli = this.cli;
+		command.init();
+		if (required) {
+			command.required = true;
+		}
+		this._commands.push(command);
+		return command;
+	}
+	public async run(): Promise<void> {
+		try {
+			this._sortByDependent();
+			for (let index in this._commands) {
+				await this._commands[index].handle.call(this._commands[index]);
+			}
+		} catch (err) {
+			this._validationErrors.push(err);
+			this._printErrors();
+			process.exit(1);
+		}
+	}
+	private _printErrors() {
+		this._validationErrors.forEach((error) => console.log(error));
+	}
+	public getCommand(name: string): InstanceType<typeof Command> {
+		const result = this._commands.filter((command) => command.getName() === name);
+		if (result.length <= 0) {
+			throw new Error(`Command with name: ${name} not found!`);
+		}
+		return result[0];
+	}
 
-  public _sortByDependent() {
-    this._commands.sort((a, b) => {
-      if (a.dependent !== null && a.dependent.getName() === b.getName()) {
-        return 1;
-      } else if (
-        b.dependent !== null &&
-        b.dependent.getName() === a.getName()
-      ) {
-        return -1;
-      } else {
-        return 0;
-      }
-    });
-  }
+	public _sortByDependent() {
+		this._commands.sort((a, b) => {
+			if (a.dependent !== null && a.dependent.getName() === b.getName()) {
+				return 1;
+			} else if (b.dependent !== null && b.dependent.getName() === a.getName()) {
+				return -1;
+			} else {
+				return 0;
+			}
+		});
+	}
 
-  public getValues() {
-    return Object.assign(
-      {},
-      this._commands.reduce((acc, command) => {
-        acc[command.getName()] = command.getValue();
-        return acc;
-      }, Object.create(null))
-    );
-  }
+	public getValues() {
+		return Object.assign(
+			{},
+			this._commands.reduce((acc, command) => {
+				acc[command.getName()] = command.getValue();
+				return acc;
+			}, Object.create(null))
+		);
+	}
 }
--- src/app.ts
+++ src/app.ts
@@ -1,149 +1,146 @@
-import { Cli } from "./Cli";
-import * as execa from "execa";
-import * as fsx from "fs-extra";
-import * as path from "path";
-const Table = require("cli-table");
+import { Cli } from './Cli';
+import * as execa from 'execa';
+import * as fsx from 'fs-extra';
+import * as path from 'path';
+import Table from 'cli-table';
 
 const log = console.log;
+
 export async function main() {
-  const app = new Cli();
-  const inputCommand = app
-    .add(false, "input", "i", "string")
-    .setValidator(async (self) => {
-      await fsx.access(self.getInputValue());
-      const pathToRepo = path.resolve(self.getInputValue(), ".git");
-      await fsx.access(pathToRepo);
-    })
-    .setTransformerValue((value) => {
-      return path.resolve(value);
-    });
-  app.add(false, "plain", "p", "boolean");
-  app.add(false, "json", "j", "boolean");
-  app
-    .add(false, "for-commit", "f", "string")
-    .setValidator(async (self) => {
-      try {
-        const commitHash = self.getInputValue();
-        await execa(`git cat-file -e ${commitHash}`, {
-          cwd: self.dependent.getValue(),
-        });
-      } catch (err) {
-        log(err);
-        throw new Error(`Commit ${self.getInputValue()} not found!`);
-      }
-    })
-    .setTransformerValue((value) => {
-      return value || null;
-    })
-    .dependsOn(inputCommand);
-  await app.run();
+	const app = new Cli();
+	const inputCommand = app
+		.add(false, 'input', 'i', 'string')
+		.setValidator(async (self) => {
+			await fsx.access(self.getInputValue());
+			const pathToRepo = path.resolve(self.getInputValue(), '.git');
+			await fsx.access(pathToRepo);
+		})
+		.setTransformerValue((value) => {
+			return path.resolve(value);
+		});
+	app.add(false, 'plain', 'p', 'boolean');
+	app.add(false, 'json', 'j', 'boolean');
+	app.add(false, 'for-commit', 'f', 'string')
+		.setValidator(async (self) => {
+			try {
+				const commitHash = self.getInputValue();
+				await execa(`git cat-file -e ${commitHash}`, {
+					cwd: self.dependent.getValue()
+				});
+			} catch (err) {
+				log(err);
+				throw new Error(`Commit ${self.getInputValue()} not found!`);
+			}
+		})
+		.setTransformerValue((value) => {
+			return value || null;
+		})
+		.dependsOn(inputCommand);
+	await app.run();
 
-  try {
-    let projectPath = app.getCommand("input").getValue();
-    const isPlain = app.getCommand("plain").getValue();
-    const isJson = app.getCommand("json").getValue();
-    const hash = app.getCommand("for-commit").getValue();
-    if (!projectPath) {
-      projectPath = path.resolve(process.cwd(), ".git");
-      log(projectPath);
-      try {
-        await fsx.access(projectPath);
-      } catch (err) {
-        return log(
-          "There is no .git directory in current directory. Please use --input[-i] option if it in another place"
-        );
-      }
-    }
+	try {
+		let projectPath = app.getCommand('input').getValue();
+		const isPlain = app.getCommand('plain').getValue();
+		const isJson = app.getCommand('json').getValue();
+		const hash = app.getCommand('for-commit').getValue();
+		if (!projectPath) {
+			projectPath = path.resolve(process.cwd(), '.git');
+			log(projectPath);
+			try {
+				await fsx.access(projectPath);
+			} catch (err) {
+				return log(
+					'There is no .git directory in current directory. Please use --input[-i] option if it in another place'
+				);
+			}
+		}
 
-    let gitShowResult;
-    if (hash) {
-      gitShowResult = await execa(
-        "xargs",
-        [
-          "git",
-          "--no-pager",
-          "show",
-          "--decorate=short",
-          "--relative",
-          '--pretty=format:">>>%ae|%s|%h"',
-          "--name-only",
-        ],
-        { input: hash, cwd: projectPath }
-      );
-    } else {
-      let gitLogResult = execa(
-        "git",
-        ["--no-pager", "log", "--decorate=short", '--pretty=format:"%H"'],
-        { stdout: "pipe", cwd: projectPath }
-      );
-      gitShowResult = await execa(
-        "xargs",
-        [
-          "git",
-          "--no-pager",
-          "show",
-          "--decorate=short",
-          "--relative",
-          '--pretty=format:">>>%ae|%s|%h|%cr"',
-          "--name-only",
-        ],
-        { input: gitLogResult.stdout, cwd: projectPath }
-      );
-    }
-    let spliitedbyCommits = gitShowResult.stdout
-      .split(">>>")
-      .slice(1)
-      .map((commit) => {
-        let newCommit = { author: "", name: "", files: [], hash: "", date: "" };
-        const [msg, ...files] = commit.split("\n");
-        const [author, name, hash, date] = msg.split("|");
-        newCommit.author = author;
-        newCommit.name = name;
-        newCommit.files = files;
-        newCommit.hash = hash.replace('"', "");
-        newCommit.date = date;
-        return newCommit;
-      });
+		let gitShowResult;
+		if (hash) {
+			gitShowResult = await execa(
+				'xargs',
+				[
+					'git',
+					'--no-pager',
+					'show',
+					'--decorate=short',
+					'--relative',
+					'--pretty=format:">>>%ae|%s|%h"',
+					'--name-only'
+				],
+				{ input: hash, cwd: projectPath }
+			);
+		} else {
+			let gitLogResult = execa('git', ['--no-pager', 'log', '--decorate=short', '--pretty=format:"%H"'], {
+				stdout: 'pipe',
+				cwd: projectPath
+			});
+			gitShowResult = await execa(
+				'xargs',
+				[
+					'git',
+					'--no-pager',
+					'show',
+					'--decorate=short',
+					'--relative',
+					'--pretty=format:">>>%ae|%s|%h|%cr"',
+					'--name-only'
+				],
+				{ input: gitLogResult.stdout, cwd: projectPath }
+			);
+		}
+		let spliitedbyCommits = gitShowResult.stdout
+			.split('>>>')
+			.slice(1)
+			.map((commit) => {
+				let newCommit = {
+					author: '',
+					name: '',
+					files: [],
+					hash: '',
+					date: ''
+				};
+				const [msg, ...files] = commit.split('\n');
+				const [author, name, hash, date] = msg.split('|');
+				newCommit.author = author;
+				newCommit.name = name;
+				newCommit.files = files;
+				newCommit.hash = hash.replace('"', '');
+				newCommit.date = date;
+				return newCommit;
+			});
 
-    if (isPlain) {
-      log(
-        spliitedbyCommits.forEach((commit) =>
-          log(
-            `${commit.author}\t${commit.name}\t${commit.files.length}\t${commit.hash}\t${commit.date}`
-          )
-        )
-      );
-    } else if (isJson) {
-      log(
-        JSON.stringify(
-          spliitedbyCommits.map((commit) => ({
-            name: commit.name,
-            author: commit.author,
-            files: commit.files.length,
-            hash: commit.hash,
-            date: commit.date,
-          })),
-          null,
-          2
-        )
-      );
-    } else {
-      const table = new Table({
-        head: ["Author", "Commit", "Changed Files", "hash", "date"],
-        colWidths: [50, 50, 13, 10, 20],
-      });
-      spliitedbyCommits.forEach((commit) =>
-        table.push([
-          commit.author,
-          commit.name,
-          commit.files.length,
-          commit.hash,
-          commit.date,
-        ])
-      );
-      log(table.toString());
-    }
-  } catch (err) {
-    log(err);
-  }
+		if (isPlain) {
+			log(
+				spliitedbyCommits.forEach((commit) =>
+					log(`${commit.author}\t${commit.name}\t${commit.files.length}\t${commit.hash}\t${commit.date}`)
+				)
+			);
+		} else if (isJson) {
+			log(
+				JSON.stringify(
+					spliitedbyCommits.map((commit) => ({
+						name: commit.name,
+						author: commit.author,
+						files: commit.files.length,
+						hash: commit.hash,
+						date: commit.date
+					})),
+					null,
+					2
+				)
+			);
+		} else {
+			const table = new Table({
+				head: ['Author', 'Commit', 'Changed Files', 'hash', 'date'],
+				colWidths: [50, 50, 13, 10, 20]
+			});
+			spliitedbyCommits.forEach((commit) =>
+				table.push([commit.author, commit.name, commit.files.length, commit.hash, commit.date])
+			);
+			log(table.toString());
+		}
+	} catch (err) {
+		log(err);
+	}
 }
